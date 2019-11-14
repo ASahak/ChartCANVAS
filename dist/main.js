2bc893317934129607b1021946d38717dccf25fd
ASahak <arthur.sahakyan96@gmail.com>
2019-11-14 10:05:17 +0400

    added gradient functional and thin line

--- dist/main.js
+++ dist/main.js
@@ -95,7 +95,7 @@
 
 exports = module.exports = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js")(false);
 // Module
-exports.push([module.i, "#chart{\r\n    border:1px solid #ccc;\r\n}", ""]);
+exports.push([module.i, "#chart{\r\n    border:1px solid #000;\r\n}", ""]);
 
 
 /***/ }),
@@ -548,9 +548,26 @@ function Chart (selector) {
     function Bar (options) {
         this._configuration = options
     }
+    Bar.prototype.__setAxisYLine = function () {
+        self._canvas.beginPath();
+        self._canvas.translate(0.5,0.5),
+        self._lineYWidth && (
+            self._canvas.moveTo(self._paddingYLeft, self._heightCanvas - self._paddingXBottom),
+            self._canvas.lineTo(self._paddingYLeft, self._paddingXTop),
+            self._canvas.lineWidth = self._lineYWidth
+        )
+        if (self._borderColor instanceof Array) {
+            self._canvas.strokeStyle = self._result.__drawLine(self._paddingYLeft, self._heightCanvas - self._paddingXBottom, self._paddingYLeft, self._paddingXTop, self._borderColor)
+        } else {
+            console.log(self._borderColor)
+            self._canvas.strokeStyle = self._borderColor
+        }
+        self._canvas.stroke()
+    }
     Bar.prototype.__setAxisY = function () {
         let _values = this._configuration.data.datasets.data.map(_ => _.value)
         console.log(this._configuration, Math.max(..._values))
+        this.__setAxisYLine()
     }
     Bar.prototype.__setAxisX = function () {
         self._canvas.font = '14px Arial';
@@ -570,11 +587,29 @@ function Chart (selector) {
     }
     // ****************************
 
-    Result.prototype.init = function (options) {
-        selector.width = self._widthCanvas;
-        selector.height = self._heightCanvas;
-        self._result = new Result(selector, options);
-        return self._result[options.type]
+    /******************HELPERS***********/
+    Result.prototype.__drawLine = function (xMoveTo, yMoveTo, xLineTo, yLineTo, dataColor) {
+        const gradient = self._canvas.createLinearGradient(xMoveTo, yMoveTo, xLineTo, yLineTo);
+        let grow = 1 / (dataColor.length - 1);
+        dataColor.forEach((_, index)=> {
+            gradient.addColorStop( grow * index, _);
+        })
+        return gradient
+    }
+    /*_________________________________*/
+
+    Result.prototype.init = function (parameters) {
+        selector.width                = self._widthCanvas;
+        selector.height               = self._heightCanvas;
+        self._paddingYLeft            = parameters.options.padding && parameters.options.padding.paddingLeft || 10;
+        self._paddingXBottom          = parameters.options.padding && parameters.options.padding.paddingBottom || 10;
+        self._paddingYRight           = parameters.options.padding && parameters.options.padding.paddingRight || 10;
+        self._paddingXTop             = parameters.options.padding && parameters.options.padding.paddingTop || 10;
+        self._lineYWidth              = parameters.options.scales && parameters.options.scales.yAxis.lineWidth || null;
+        self._lineXWidth              = parameters.options.scales && parameters.options.scales.xAxis.lineWidth || null;
+        self._borderColor             = parameters.data.datasets.borderColor && parameters.data.datasets.borderColor || '#000';
+        self._result = new Result(selector, parameters);
+        return self._result[parameters.type]
     }
     return new Result(selector)
 }
@@ -584,11 +619,24 @@ Chart(canvas).init({
         labels: _dataChart['2019'].map(_ => _.text),
         datasets: {
             data: _dataChart['2019'],
+            borderColor: ['red', 'blue']
         }
     },
-    options: {}
+    options: {
+        padding: {
+            paddingLeft: 30,
+            paddingBottom: 30
+        },
+        scales: {
+            yAxis: {
+                lineWidth: 1
+            },
+            xAxis: {
+                lineWidth: 1
+            }
+        }
+    }
 })
-
 
 /***/ }),
 
--- src/index.js
+++ src/index.js
@@ -45,9 +45,26 @@ function Chart (selector) {
     function Bar (options) {
         this._configuration = options
     }
+    Bar.prototype.__setAxisYLine = function () {
+        self._canvas.beginPath();
+        self._canvas.translate(0.5,0.5),
+        self._lineYWidth && (
+            self._canvas.moveTo(self._paddingYLeft, self._heightCanvas - self._paddingXBottom),
+            self._canvas.lineTo(self._paddingYLeft, self._paddingXTop),
+            self._canvas.lineWidth = self._lineYWidth
+        )
+        if (self._borderColor instanceof Array) {
+            self._canvas.strokeStyle = self._result.__drawLine(self._paddingYLeft, self._heightCanvas - self._paddingXBottom, self._paddingYLeft, self._paddingXTop, self._borderColor)
+        } else {
+            console.log(self._borderColor)
+            self._canvas.strokeStyle = self._borderColor
+        }
+        self._canvas.stroke()
+    }
     Bar.prototype.__setAxisY = function () {
         let _values = this._configuration.data.datasets.data.map(_ => _.value)
         console.log(this._configuration, Math.max(..._values))
+        this.__setAxisYLine()
     }
     Bar.prototype.__setAxisX = function () {
         self._canvas.font = '14px Arial';
@@ -67,11 +84,29 @@ function Chart (selector) {
     }
     // ****************************
 
-    Result.prototype.init = function (options) {
-        selector.width = self._widthCanvas;
-        selector.height = self._heightCanvas;
-        self._result = new Result(selector, options);
-        return self._result[options.type]
+    /******************HELPERS***********/
+    Result.prototype.__drawLine = function (xMoveTo, yMoveTo, xLineTo, yLineTo, dataColor) {
+        const gradient = self._canvas.createLinearGradient(xMoveTo, yMoveTo, xLineTo, yLineTo);
+        let grow = 1 / (dataColor.length - 1);
+        dataColor.forEach((_, index)=> {
+            gradient.addColorStop( grow * index, _);
+        })
+        return gradient
+    }
+    /*_________________________________*/
+
+    Result.prototype.init = function (parameters) {
+        selector.width                = self._widthCanvas;
+        selector.height               = self._heightCanvas;
+        self._paddingYLeft            = parameters.options.padding && parameters.options.padding.paddingLeft || 10;
+        self._paddingXBottom          = parameters.options.padding && parameters.options.padding.paddingBottom || 10;
+        self._paddingYRight           = parameters.options.padding && parameters.options.padding.paddingRight || 10;
+        self._paddingXTop             = parameters.options.padding && parameters.options.padding.paddingTop || 10;
+        self._lineYWidth              = parameters.options.scales && parameters.options.scales.yAxis.lineWidth || null;
+        self._lineXWidth              = parameters.options.scales && parameters.options.scales.xAxis.lineWidth || null;
+        self._borderColor             = parameters.data.datasets.borderColor && parameters.data.datasets.borderColor || '#000';
+        self._result = new Result(selector, parameters);
+        return self._result[parameters.type]
     }
     return new Result(selector)
 }
@@ -81,7 +116,21 @@ Chart(canvas).init({
         labels: _dataChart['2019'].map(_ => _.text),
         datasets: {
             data: _dataChart['2019'],
+            borderColor: ['red', 'blue']
         }
     },
-    options: {}
+    options: {
+        padding: {
+            paddingLeft: 30,
+            paddingBottom: 30
+        },
+        scales: {
+            yAxis: {
+                lineWidth: 1
+            },
+            xAxis: {
+                lineWidth: 1
+            }
+        }
+    }
 })
